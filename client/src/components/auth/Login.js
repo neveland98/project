52525996a14d09801e6208989df8f9655fea51c6
mkjovan <markoklugejovanovic@gmail.com>
2019-11-29 01:14:35 -0600

    added styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import {Paper} from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey';
 class Login extends Component {
   constructor() {
     super();
@@ -44,73 +46,73 @@ this.props.loginUser(userData); // since we handle the redirect within our compo
 render() {
     const { errors } = this.state;
 return (
-      <div className="container">
-        <div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
+      <div className="container" style = {{backgroundColor:'#eeeeee', width: "100vw", height: "100vh"}}>
             <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
+                <i className="material-icons left">keyboard_backspace</i> Back to
+                home
             </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.name || errors.namenotfound
-                  })}
-                />
-                <label htmlFor="name">name</label>
-                <span className="red-text">
-                  {errors.name}
-                  {errors.namenotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+            <Paper style={{width:"80%", margin: "auto"}}>
+                <div style={{ marginTop: "4rem"}} className="row">
+                    <div className="col s8 offset-s2">
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                        <h4>
+                            <b>Login</b> below
+                        </h4>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                        <div className="input-field col s12">
+                            <input
+                            onChange={this.onChange}
+                            value={this.state.name}
+                            error={errors.name}
+                            id="name"
+                            type="email"
+                            className={classnames("", {
+                                invalid: errors.name || errors.namenotfound
+                            })}
+                            />
+                            <label htmlFor="name">Username</label>
+                            <span className="red-text">
+                            {errors.name}
+                            {errors.namenotfound}
+                            </span>
+                        </div>
+                        <div className="input-field col s12">
+                            <input
+                            onChange={this.onChange}
+                            value={this.state.password}
+                            error={errors.password}
+                            id="password"
+                            type="password"
+                            className={classnames("", {
+                                invalid: errors.password || errors.passwordincorrect
+                            })}
+                            />
+                            <label htmlFor="password">Password</label>
+                            <span className="red-text">
+                            {errors.password}
+                            {errors.passwordincorrect}
+                            </span>
+                        </div>
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <button
+                            style={{
+                                width: "150px",
+                                borderRadius: "3px",
+                                letterSpacing: "1.5px",
+                                marginTop: "1rem",
+                                marginBottom: "1rem"
+                            }}
+                            type="submit"
+                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                            >
+                            Login
+                            </button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </Paper>
       </div>
     );
   }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,34 +7,14 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              Login into the admin portal
-            </h4>
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
+          <Link
                 to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
+                >
+              
+                Log in to the admin portal
               </Link>
-            </div>
+            
+            </h4>
           </div>
         </div>
       </div>
