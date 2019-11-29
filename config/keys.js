544a2a88778424a75f839fac24daf5ec24438e17
mkjovan <markoklugejovanovic@gmail.com>
2019-11-29 00:23:49 -0600

    reworked website

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:admin@cluster0-2gh0b.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: String,
+  password: String
+});
+module.exports = User = mongoose.model("users", UserSchema);
